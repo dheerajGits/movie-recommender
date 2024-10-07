3fa6260b3683de17d8f4312382d32837d1e3a5b4
Dheeraj202 <dheerajsadan2002@gmail.com>
2024-10-08 00:43:28 +0530

    add similarity.pkl and add streamlit frontend

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 import pickle
+import pandas as pd
 
-movies_list= pickle.load(open('model/movies.pkl','rb'))
+def recommend(movie):
+    movie_index= movies[movies['title'] == movie].index[0]
+    distances= similarity_array[movie_index] 
+    movies_list= sorted(list(enumerate(distances)),reverse=True,key= lambda x:x[1])[1:6]
+    recommended_movies=[]
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+        
+
+movies_list= pickle.load(open('model/movies_dict.pkl','rb'))
+similarity_array= pickle.load(open('model/similarity.pkl','rb'))
+movies= pd.DataFrame(movies_list)
 st.title('Movie Recommender System')
 
-option= st.selectbox("Select the movie to be get recommendations",('Email','Phone','Home Phone'))
+selected_movie= st.selectbox("Select the movie to be get recommendations",movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations= recommend(selected_movie )
+    for i in recommendations: 
+        st.write(i)
